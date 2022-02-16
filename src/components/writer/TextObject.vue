221589001ed34b46d6b0489f9f57b890434430ce
HoCG <rhwkd5012@naver.com>
2022-02-16 10:10:52 +0900

    제출페이지 화면구성

---
+++ src/components/writer/TextObject.vue
@@ -0,0 +1,101 @@
+<template>
+  <div class="text-object-wrapper"
+  :style="positionObject"
+  >
+      <div class="object-name">
+        {{object.name}}
+      </div>
+      <div class="object-box"
+      :id="'object_' + this.propKey"
+      :style="shapeObject"
+      >
+      </div>
+  </div>
+</template>
+
+<script>
+import {objectMixin} from "@/mixins/objectMixin"
+export default {
+
+
+  mixins: [objectMixin],
+
+  props: {
+    object: Object,
+    propKey: Number
+  },
+
+  mounted(){
+
+    this.positionObject = {
+      top: `${this.object.y_position}px`,
+      left: `${this.object.x_position}px`,
+    };
+
+    this.shapeObject = {
+      width: `${this.object.width}px`,
+      height: `${this.object.height}px`,
+    }
+
+    let ResizeSensor = require('css-element-queries/src/ResizeSensor');
+    const box = document.getElementById('object_' + this.propKey);
+    new ResizeSensor(box, (e) => {
+      this.shapeObject = {
+        width: e.width,
+        height: e.height
+      }
+
+      this.$store.commit("UPDATE_TEXT_OBJECT", {
+        ...this.object,
+        width: this.shapeObject.width,
+        height: this.shapeObject.height
+      })
+    })
+  }
+
+}
+</script>
+
+<style lang="scss" scoped>
+
+.text-object-wrapper{
+  position: absolute;
+  opacity: 0.999;
+  top: 150px;
+  left: 100px;
+  cursor: pointer;
+
+  .object-name{
+    color: #5c5c5c;
+    font-size: 12px;
+    font-weight: bold;
+  }
+
+  .object-box{
+    position: absolute;
+    width: 100px;
+    height: 100px;
+    background-color: #DADADA;
+    border: 1px solid #767676;
+    resize: both;
+    overflow: hidden;
+    background-image: url('../../images/underline.png');
+  }
+  .button-wrapper{
+    width: 30px;
+    height: 30px;
+    position: absolute;
+    top: 0px;
+    right: 0px;
+  }
+  .x-button{
+    position:absolute;
+    top: calc(50% - 20px);
+    left: calc(50% + 0px);
+    z-index: 100;
+    width: 20px;
+    height: 20px;
+  }
+}
+
+</style>
